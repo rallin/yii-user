e7153872d80532c5eba2e11b9fa2d9145e558bbf
Kirill Dubelevich <kirill.dubelevich@gmail.com>
2012-04-06 16:26:17 +0300

    Fixed bug: throw exception when profile's field is null

--- views/admin/view.php
+++ views/admin/view.php
@@ -26,11 +26,12 @@ $this->menu=array(
 	$profileFields=ProfileField::model()->forOwner()->sort()->findAll();
 	if ($profileFields) {
 		foreach($profileFields as $field) {
+            $value = $model->profile->getAttribute($field->varname);
 			array_push($attributes,array(
 					'label' => UserModule::t($field->title),
 					'name' => $field->varname,
 					'type'=>'raw',
-					'value' => (($field->widgetView($model->profile))?$field->widgetView($model->profile):(($field->range)?Profile::range($field->range,$model->profile->getAttribute($field->varname)):$model->profile->getAttribute($field->varname))),
+					'value' => (($field->widgetView($model->profile))?$field->widgetView($model->profile):(($field->range)?Profile::range($field->range,$value):(isset($value)?$value:''))),
 				));
 		}
 	}
